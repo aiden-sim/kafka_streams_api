e66f4c2046005ea4caa7c66c957130fe4202dd80
aiden <s860620@gmail.com>
2019-03-23 00:57:35 +0900

    first

---
+++ src/main/java/myapps/Pipe.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package myapps;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * In this example, we implement a simple LineSplit program using the high-level Streams DSL
+ * that reads from a source topic "streams-plaintext-input", where the values of messages represent lines of text,
+ * and writes the messages as-is into a sink topic "streams-pipe-output".
+ */
+public class Pipe {
+
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        // 카프카 클러스터 내의 스트림즈 애플리케이션을 구분할 때 사용하는 것이므로 유일한 값이어야 한다.
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
+        // 스트림즈 애플리케이션이 접근할 브로커의 아이피와 포트
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        // 키 문자열 타입을 받을 수 있는 Serdes.String() 지정
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        // 토플로지 만드는 과정
+
+        final StreamsBuilder builder = new StreamsBuilder();
+        // streams-plaintext-input -> streams-pipe-output 으로 전달
+        builder.stream("streams-plaintext-input").to("streams-pipe-output");
+
+        // 최종적인 토플로지를 만들기 위해서, build 사용
+        final Topology topology = builder.build();
+        // 토플로지 확인
+        System.out.println(topology.describe());
+
+        /*final KafkaStreams streams = new KafkaStreams(topology, props);
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        // attach shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        } catch (Throwable e) {
+            System.exit(1);
+        }
+        System.exit(0);*/
+    }
+}
